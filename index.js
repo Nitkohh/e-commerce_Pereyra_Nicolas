4ddeb44f77427d6e90a19db0de87a8fb05eb7ddf
Nicolas <pereyranicolas417@gmail.com>
2024-09-17 19:29:42 -0300

    Arreglos2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("Hello Word");
 console.log (1)
+//Tarea queryselector
+const nombre1 = prompt('Ingresa el primer nombre:');
+const nombre2 = prompt('Ingresa el segundo nombre:');
+const nombre3 = prompt('Ingresa el tercer nombre:');
+
+document.querySelector('.card1 .card-title').innerText = nombre1;
+document.querySelector('.card2 .card-title').innerText = nombre2;
+document.querySelector('.card3 .card-title').innerText = nombre3;
